eb4d04e936dce8e3a4c5893cd0b1acdd1578e0e1
LYK <dalinaum@gmail.com>
2023-03-29 17:25:06 +0900

    상대강도와 미너비니 템플릿 계산

--- calc-rs.py
+++ calc-rs.py
@@ -53,7 +53,20 @@ quater = 21 * 3
 # https://www.investopedia.com/ask/answers/021015/how-can-you-calculate-volatility-excel.asp
 # https://www.investopedia.com/articles/06/historicalvolatility.asp
 
-rs_df = pd.DataFrame(columns=['Code', 'Name', 'Score', 'YesterdayScore', 'Close1', 'Close2'])
+rs_df = pd.DataFrame(columns=[
+    'Code',
+    'Name',
+    'Score',
+    'YesterdayScore',
+    'Close1',
+    'Close2',
+    'MA50',
+    'MA150',
+    'MA200',
+    'LastMonthMA200',
+    'Min52W',
+    'Max52W'
+])
 
 
 def calc_score(data, day=-1):
@@ -89,15 +102,42 @@ for i in kospi_list.itertuples():
     if today_score != -1:
         today = data.loc[data.index[-1]]
         four_quarter_ago = data.loc[data.index[-1 - (quater * 4)]]
-        rs_df = rs_df.append(
-            {'Code': i.Code, 'Name': i.Name, 'Score': today_score, 'YesterdayScore': yesterday_score, 'Close1': four_quarter_ago.Close, 'Close2': today.Close}, ignore_index=True)
+
+        data_260 = data.tail(260)
+        data_260_close = data_260.Close
+        max_52w = data_260_close.max()
+        min_52w = data_260_close.min()
+        data_220_close = data_260_close.tail(220)
+        last_month_ma_200 = int(data_220_close.head(200).mean())
+        data_200_close = data_220_close.tail(200)
+        ma_200 = int(data_200_close.mean())
+        data_150_close = data_200_close.tail(150)
+        ma_150 = int(data_150_close.mean())
+        data_50_close = data_150_close.tail(50)
+        ma_50 = int(data_50_close.mean())
+
+        rs_df = rs_df.append({
+            'Code': i.Code,
+            'Name': i.Name,
+            'Score': today_score,
+            'YesterdayScore': yesterday_score,
+            'Close1': four_quarter_ago.Close,
+            'Close2': today.Close,
+            'MA50': ma_50,
+            'MA150': ma_150,
+            'MA200': ma_200,
+            'LastMonthMA200': last_month_ma_200,
+            'Min52W': min_52w,
+            'Max52W': max_52w,
+        }, ignore_index=True)
     print(f"today score: {today_score} / yesterday score: {yesterday_score}")
 
 rs_df['Rank'] = rs_df['Score'].rank()
 rs_df['RS'] = (rs_df['Rank'] * 98 / len(rs_df)).apply(np.int64) + 1
 
 rs_df['YesterdayRank'] = rs_df['YesterdayScore'].rank()
-rs_df['YesterdayRS'] = (rs_df['YesterdayRank'] * 98 / len(rs_df)).apply(np.int64) + 1
+rs_df['YesterdayRS'] = (rs_df['YesterdayRank'] * 98 /
+                        len(rs_df)).apply(np.int64) + 1
 
 na_index = rs_df['YesterdayRS'].isna()
 rs_df['RankChange'] = rs_df['RS'] - rs_df['YesterdayRS']
@@ -114,9 +154,7 @@ with open(result_file_path, "w") as f:
     layout: single
     '''
     f.write(textwrap.dedent(header_start))
-    # title:  "코스피 상대강도 2023년 3월 22일"
     f.write(now.strftime('title: "코스피 상대강도 %Y년 %-m월 %-d일"\n'))
-    # 2023-03-23 01:21:00 +0900
     f.write(now.strftime("date: %Y-%m-%d %H:%M:%S +0900\n"))
     header_end = '''\
     categories: rs
@@ -131,8 +169,8 @@ with open(result_file_path, "w") as f:
 
     ## 코스피 상대강도
     
-    |종목코드|이름|종가 1|종가 2|상대강도 (전일대비)|
-    |------|---|-----|-----|--------------|
+    |종목코드|이름|1년 전|종가|상대강도|
+    |------|---|-----|--|------|
     '''
     f.write(textwrap.dedent(comment))
 
@@ -143,4 +181,54 @@ with open(result_file_path, "w") as f:
             change = f"(+{i.RankChange})"
         else:
             change = f"({i.RankChange})"
-        f.write(f"|{i.Code}|{i.Name}|{i.Close1}|{i.Close2}|{i.RS} {change}|\n")
+        f.write(
+            f"|{i.Code}|{i.Name}|{i.Close1}|{i.Close2}|{i.RS} {change}|\n")
+
+
+result_file_path = os.path.join(
+    posts_dir, f"{date}-trending-template.markdown")
+
+minervini = sorted[sorted.RS >= 70]
+minervini = minervini[minervini.Close2 > minervini.MA50]
+minervini = minervini[minervini.Close2 > minervini.MA150]
+minervini = minervini[minervini.Close2 > minervini.MA200]
+minervini = minervini[minervini.MA50 > minervini.MA150]
+minervini = minervini[minervini.MA150 > minervini.MA200]
+minervini = minervini[minervini.MA200 > minervini.LastMonthMA200]
+minervini = minervini[minervini.Close2 > minervini.Min52W * 1.3]
+minervini = minervini[minervini.Close2 > minervini.Max52W * 0.75]
+
+# Close > MA50, Close > MA150, Close > MA200
+# MA50 > MA150 > MA200
+# 1달전 MA200 < 오늘 MA200
+# Close > 52-low * 1.3
+# Close <= 52-high * 0.75
+# RS 70점 이상
+
+with open(result_file_path, "w") as f:
+    header_start = '''\
+    ---
+    layout: single
+    '''
+    f.write(textwrap.dedent(header_start))
+    f.write(now.strftime('title: "미너비니 트렌드 템플릿 %Y년 %-m월 %-d일"\n'))
+    f.write(now.strftime("date: %Y-%m-%d %H:%M:%S +0900\n"))
+    header_end = '''\
+    categories: minervini
+    ---
+    '''
+    f.write(textwrap.dedent(header_end))
+
+    comment = '''\
+    마크 미니버니(Mark Minervini)의 트렌드 템플릿(Trend Template)을 계산합니다.
+
+    ## 미너비니 트렌드 템플릿
+    
+    |종목코드|이름|종가|RS|신고가,신저가|MA50,150,200|
+    |------|---|---|--|---------|------------|
+    '''
+    f.write(textwrap.dedent(comment))
+
+    for i in minervini.itertuples():
+        f.write(
+            f"|{i.Code}|{i.Name}|{i.Close2}|{i.RS}|{i.Max52W},{i.Min52W}|{i.MA50},{i.MA150},{i.MA200}|\n")
